0c19bfa673fa7299bc14447504aee2e29169066f
pawarsaagar9 <pawar.saagar9@gmail.com>
2025-09-25 23:30:05 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,1 +1,21 @@
 
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { AzureQueryBuilderComponent } from './azure-query-builder.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AzureQueryBuilderComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
